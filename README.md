77620c3b05709baf55a4dab1cdade0b26840af34
Alvin Albert Michael <113367440+alvin-dotcom@users.noreply.github.com>
2023-11-21 12:41:23 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Music_Player
+This is a music player made with using  HTML,CSS3 and Javascript
+![image](https://github.com/alvin-dotcom/Music_Player/assets/113367440/37c85baa-5399-4456-8d51-3d13829d79a8)
---
+++ music-list.js
@@ -0,0 +1,36 @@
+let allMusic=[
+    {
+        name: "Tu Hai kahan",
+        artist: "Uraan",
+        img: "image-1",
+        src: "music-1"
+    },
+    {
+        name: "One Love",
+        artist: "Shubh",
+        img: "image-2",
+        src: "music-2"
+    },
+    {
+        name: "Chaleya",
+        artist: "Arijit Singh,Shilpa Rao",
+        img: "image-3",
+        src: "music-3"
+    },
+    {
+        name: "Steal My Girl",
+        artist: "One Direction",
+        img: "image-4",
+        src: "music-4"
+    },
+    {
+        name: "With You",
+        artist: "AP Dhillon",
+        img: "image-5",
+        src: "music-5"
+    },
+   
+    
+   
+    
+]
